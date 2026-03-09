9bc4b7ea08123a17dd2acbf0b825b3686c4b8b9e
Shivasankarisharmi <shivasankarisharmi@gmail.com>
2026-03-09 20:18:09 +0530

    fix error

--- health-app-frontend/src/services/api.js
+++ health-app-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://healthapp-wlzt.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
@@ -13,13 +13,13 @@ API.interceptors.request.use((req) => {
 });
 
 API.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
       localStorage.removeItem("token");
       window.location.href = "/login";
     }
-    return Promise.reject(error);
+    return Promise.reject(err);
   }
 );
 
